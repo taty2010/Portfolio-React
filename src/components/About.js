c7f69728c127e794250674c819c47148b438a6c6
taty2010 <shydalys123@gmail.com>
2019-09-18 19:40:50 -0500

    Added more to the about me paragraph

--- src/components/About.js
+++ src/components/About.js
@@ -4,7 +4,11 @@ function About() {
   const AboutText = (
     <div className="aboutTitle">
       <h2>Hello,</h2>
-      <p>My name is Tatyana Novell and I am currently attending Lambda School to become a Software Engineer</p>
+      <p>My name is Tatyana Novell, I am a Web Content Editor
+         currently attending Lambda School to become a Software Engineer.
+         Checkout what I've learned so far and take a peak at all the projects I've completed.
+
+      </p>
     </div>
   );
   return <div className="about">{AboutText}</div>;
