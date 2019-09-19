c627804af30547ab09f23d81db1d852804abd962
taty2010 <shydalys123@gmail.com>
2019-09-19 18:23:16 -0500

    changes

--- src/components/About.js
+++ src/components/About.js
@@ -8,7 +8,7 @@ function About() {
          currently attending Lambda School to become a Software Engineer.
          Checkout what I've learned so far and take a peak at all the projects I've completed.
       </p>
-      <i class="fas fa-portrait"></i>
+      <i className="fas fa-portrait"></i>
     </div>
   );
   return <div className="about">{AboutText}</div>;
