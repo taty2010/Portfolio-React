bb9f13ed5823fbbd7d454cae200a17ad1e5140b4
taty2010 <shydalys123@gmail.com>
2019-09-18 19:00:21 -0500

    Added React to the skills list

--- src/components/Skills.js
+++ src/components/Skills.js
@@ -3,26 +3,33 @@ import React from "react";
 function Skills() {
   const css = (
     <div className ="skillBox">
-        <span class="icon style2 major fa-css3"></span>
+        <i class="fab fa-css3-alt"></i>
         <h3>CSS</h3>
     </div>
   );
   const HTML = (
     <div className ="skillBox">
-        <span class="icon solid style2 major fa-html5"></span>
+        <i class="fab fa-html5"></i>
         <h3>HTML</h3>
     </div>
   );
   const js = (
     <div className ="skillBox">
-        <i class="icon solid style2 major fa-js-square"></i>
+        <i class="fab fa-js-square"></i>
         <h3>Javascript</h3>
+    </div>
+  );
+  const react = (
+    <div className ="skillBox">
+        <i class="fab fa-react"></i>
+        <h3>React</h3>
     </div>
   );
   return <div className="skills">
   {css}
   {HTML}
   {js}
+  {react}
   </div>;
 }
 
